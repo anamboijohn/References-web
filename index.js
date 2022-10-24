c4425b7a693a16dd6332b24c607aa8e152a3ed0a
anamboijohn <anamboijohn@gmail.com>
2022-10-24 05:34:57 +0100

    fresh uploads

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let references = ()=>{
+    let text = document.querySelector("#text");
+    let reg = /([A-Z]\.?[a-z&\-’ ]{0,15})+\(\d{4}\)|\(.{1,20}\d{4}.{0,20}\)/gm;
+    return text.value.match(reg);
+}                 
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+let dsp=document.querySelector('#dsp');
+let btns=document.querySelector('#btns');
+
+let ol = document.createElement('ol');
+btns.addEventListener('click', function(){
+    removeAllChildNodes(ol);
+    let refs = references();
+    refs.forEach(ref => {
+        let li = document.createElement('li');
+           li.innerText = ref;
+           ol.append(li);
+        dsp.append(ol);
+    });
+
+})
